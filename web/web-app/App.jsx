60d34a52c3c8a08cad1b4239a18ff5aef76f334a
Mieszko Malawski <mieszko.malawski@getresponse.com>
2017-04-11 15:15:27 +0200

    product list

--- web/web-app/App.jsx
+++ web/web-app/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import rest from 'rest';
+import mime from 'rest/interceptor/mime';
+import entity from 'rest/interceptor/entity';
 
 class App extends React.Component{
 
+    componentDidMount() {
+        var endpointUrl = 'http://localhost:8080/product';
+
+        var client = rest
+            .chain(mime, { mime: 'application/json' })
+            .chain(entity);
+
+        client({ path: endpointUrl })
+            .then((response) => {
+                let tmpState = this.state;
+                tmpState.products = response.data;
+                this.setState(tmpState);
+            });
+    }
 
     constructor() {
         super();
@@ -21,17 +38,7 @@ class App extends React.Component{
     onSubmitHandler(event) {
 
         event.preventDefault();
-        var rest = require('rest');
-        var mime = require('rest/interceptor/mime');
-        var entity = require('rest/interceptor/entity');
-        var endpointUrl = 'http://localhost:8080/product';
 
-        var client = rest
-            .chain(mime, { mime: 'application/json' })
-            .chain(entity);
-
-        client({ path: endpointUrl })
-            .then((response) => {console.log(response)});
 
         let tmpState = this.state;
         tmpState.products.push({
